5149002240c37de90a456707a711236fbdad11c5
Nikos Koukis <nickkouk@gmail.com>
2016-11-07 02:45:18 +0200

    pioneer_2at to publish nav_msgs::Odometry instead of custom Pose2DStamped message

--- arduino_mr/nodes/pioneer_2at.py
+++ arduino_mr/nodes/pioneer_2at.py
@@ -2,27 +2,24 @@
 
 """
 Mon Sep 26 12:00:44 EEST 2016, Nikos Koukis
-Send velocity commands to the Arduino controlling a Pioneer 3dx robot
+Send velocity commands to the Arduino controlling a Pioneer 2at robot
 
 Initial script written by: Apostolos Poulias, 2015
 Maintainer: Nikos Koukis, 2016:-
 
 TODO:
-- Make sure that clock is not published  (e.g. by a rosbag)
 - Read serial port from the command line and have a default if not given
 """
 
 
 import rospy
 import roslaunch
-# import os
-from math import *
 from math import pi
 from math import cos, sin
 import time
 from arduino_mr.msg import arduino_input, feedback_int
-from mrpt_msgs.msg import Pose2DStamped
-from geometry_msgs.msg import Vector3Stamped
+from geometry_msgs.msg import Vector3
+from nav_msgs.msg import Odometry
 
 
 class ATX2():
@@ -38,71 +35,89 @@ class ATX2():
         self.encres = 8187.5
         # self.looptime = 15.0 * 10.0**(-3.0)
 
-        #
-        # Position estimation based only on odometry
-        #
-        self.x_total = 0
-        self.y_total = 0
-        self.theta_total = 0
+        # parameter names
+        self.param_names = {}
+        self.param_names["arduino_port"] = "port"
 
+        # frame ID names
+        self.frame_IDs = {}
+        self.frame_IDs["odometry"] = "pioneer_odom"
+        self.frame_IDs["world"] = "world" # Maybe fetch this from the param server?
 
-
+        # Message sequence nums
+        self.msg_seqs = {}
+        self.msg_seqs["odometry"] = 0
 
         self.rate = 200
-        # See http://wiki.ros.org/rospy/Overview/Publishers%20and%20Subscribers#Choosing_a_good_queue_size
+
+        # Setup the robot odometry message
+        self.odom = Odometry
+        self.odom.header.frame_id = self.frame_IDs["world"]
+        self.odom.child_frame_id = self.frame_IDs["odometry"]
+
+        self._setup_publishers_subscribers()
+        self._launch_rosserial_node()
+
+    def _setup_publishers_subscribers(self):
+
+        # See
+        # http://wiki.ros.org/rospy/Overview/Publishers%20and%20Subscribers#Choosing_a_good_queue_size
         # on how to choose queue_size
-        self.pub = rospy.Publisher('arduino_input_2at', arduino_input,
-                                   queue_size=self.rate)	
-        self.pub_velocity_odom = rospy.Publisher('/pioneer/odom',
-                                                 Vector3Stamped,
-                                                 queue_size=self.rate)	
-        self.pub_position_odom = rospy.Publisher('/pioneer/position_odom',
-                                                 Pose2DStamped,
-                                                 queue_size=self.rate)	
-
-        rospy.Subscriber("/feedback_2at", feedback_int, self.updateMROdometry)	
-
-        self._launchRosserialNode()
+        self.arduino_pub = rospy.Publisher('arduino_input_2at', arduino_input,
+                                   queue_size=self.rate)
+        self.odometry_pub = rospy.Publisher("odom",
+                                            Odometry,
+                                            queue_size=self.rate)
+        rospy.Subscriber("/feedback_2at",
+                         feedback_int,
+                         self._update_mr_odometry)
 
 
-    def _launchRosserialNode(self):
+
+    def _launch_rosserial_node(self):
         """Wrapper method for launching a rosserial node."""
         rospy.loginfo("Initializing a rosserial node...")
 
         package = "rosserial_python"
         executable = "serial_node.py"
 
-        param_arduino_port = "/arduino/port"
         num_tries = 1
         tries_thresh = 10
         rospy.loginfo(
             "Fetching the serial port for communicating with the arduino")
         # Wait until the arduino port parameter is available in the ROS
         # parameter server
-        while not rospy.has_param(param_arduino_port):
+        while not rospy.has_param(self.param_names["arduino_port"]):
             rospy.logwarn(
                 "Arduino port is not set yet. Retrying... {}/{}".format(
                     num_tries, tries_thresh))
             time.sleep(2)
 
-        serial_port = rospy.get_param("/arduino/port")
-        node = roslaunch.core.Node(package, executable, args=serial_port)
+        serial_port = rospy.get_param(self.arduino_port_param)
 
+        # Launch the rosserial node
+        node = roslaunch.core.Node(package, executable, args=serial_port)
         launch = roslaunch.scriptapi.ROSLaunch()
         launch.start()
-        launch.launch(node)
-        self.rosserial_process = rospy.loginfo(
-            "rosserial node is initialized successfully.")
+
+        self.rosserial_process = launch.launch(node)
+        rospy.loginfo("rosserial node is initialized successfully.")
 
 
     def shutdown(self):
-        for i in range(0, 100):
-            self.pub.publish(mode=3, data1=0, data2=0)
+        """
+        Safely shutdown the pioneer instance by sending bunch of 0-velocity
+        commands.
+        """
+        print 'Safe shutdown initiated'
+
+        for i in range(0, 1000):
+            self.arduino_pub.publish(mode=3, data1=0, data2=0)
             self.rate.sleep()
-        print 'Safe shutdown'
         self.rosserial_process.stop()
-      
-    def updateMROdometry(self, msg):
+
+    def _update_mr_odometry(self, msg):
+        """Update the estimated robot position and velocity."""
 
         encoderR = msg.encoderR
         encoderL = msg.encoderL
@@ -112,43 +127,30 @@ class ATX2():
         # dtheta = ((encoderR - encoderL) * 2.0 * pi * self.R) / (2.0 * self.encres * self.d)
         omega = -((wR - wL) * self.R) / (self.d * 2.0)
         u = (wR + wL) * self.R / 2.0
+        u_x = u * cos(self.odom.pose.pose.theta)
+        u_y = u * sin(self.odom.pose.pose.theta)
 
         #
-        # Robot velocity msg
+        # Update the published odometry message
         #
-        vel_msg = Vector3Stamped()
-        vel_msg.header.stamp = rospy.Time.now()
-        vel_msg.vector.x = u
-        vel_msg.vector.y = omega
-        vel_msg.vector.z = 0.0
+        self.odom.header.stamp = rospy.Time.now()
+        self.header.seq = self.msg_seqs["odometry"]
 
-        self.pub_velocity_odom.publish(vel_msg)
+        # Current velocity update
+        self.odom.twist.twist.linear = Vector3(x=u, y=0, z=0)
+        self.odom.twist.twist.angular = Vector3(x=0, y=0, z=omega)
 
-        #
-        # Robot position estimation based on odometry
-        #
+        # Current position update
+        self.odom.pose.pose.x += u_x / float(self.rate)
+        self.odom.pose.pose.y += u_y / float(self.rate)
+        self.odom.pose.pose.theta += omega / self.rate
+        self.odom.pose.pose.theta %= 2 * pi
 
-        # velocity components wrt x, y directions
-        u_x = u * cos(self.theta_total)
-        u_y = u * sin(self.theta_total)
+        # Publish the odometry message
+        rospy.logdebug("Publishing pioneer nav_msgs::Odometry message... {}".format(self.odom))
+        self.odometry_pub.publish(self.odom)
 
-        # update position estimation - 1st order integration
-        self.x_total += u_x / float(self.rate)
-        self.y_total += u_y / float(self.rate)
-
-        self.theta_total += omega / self.rate
-        self.theta_total %= (2 * pi)
-
-
-        # publish position
-        pos_msg = Pose2DStamped()
-        pos_msg.header.stamp = rospy.Time.now()
-        pos_msg.pose.x = self.x_total
-        pos_msg.pose.y = self.y_total
-        pos_msg.pose.theta = self.theta_total
-        self.pub_position_odom.publish(pos_msg)
-
-
+        self.msg_seqs["odometry"] += 1
 
 if __name__ == '__main__':
     rospy.init_node('pioneer_atx_node')
@@ -157,7 +159,6 @@ if __name__ == '__main__':
     try:
         while not rospy.is_shutdown():
             rate_it.sleep()
-        #safe_shutdown()
             rospy.spin()
     except rospy.ROSInterruptException:
         pass
